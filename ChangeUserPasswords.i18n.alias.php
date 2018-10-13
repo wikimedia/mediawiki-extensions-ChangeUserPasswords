719b830943256262e07ba4511b976318422770df
Zoranzoki21 <zorandori4444@gmail.com>
2018-10-14 01:36:24 +0200

    Add aliases for Serbian
    Change-Id: I4dca02c65f1bd2fc0064a1c24229fa2f8faccb72

--- ChangeUserPasswords.i18n.alias.php
+++ ChangeUserPasswords.i18n.alias.php
@@ -12,3 +12,13 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'ChangeUserPasswords' => [ 'Change User Passwords' , 'ChangeUserPasswords' ],
 ];
+
+/** Serbian Cyrillic (српски (ћирилица)) */
+$specialPageAliases['sr-ec'] = [
+	'ChangeUserPasswords' => [ 'Промени_лозинке_корисника' , 'ПромениЛозинкеКорисника' ],
+];
+
+/** Serbian Latin (srpski (latinica)) */
+$specialPageAliases['sr-el'] = [
+	'ChangeUserPasswords' => [ 'Promeni_lozinke_korisnika' , 'PromeniLozinkeKorisnika' ],
+];
